eb0f79c2251c922e08d397cb73245f99e91dbf1b
D1eG0-404 <dgo_xbox@live.com>
2023-10-30 01:30:23 +0000

    Initial config

---
+++ src/test/java/com/example/Hyperskill/HyperskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hyperskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperskillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
