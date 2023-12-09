fcbe4cf88c6d57c87c9f99a3bba017253d907cf9
D1eG0-404 <dgo_xbox@live.com>
2023-12-09 18:54:57 +0000

    Fix this class

---
+++ src/main/java/com/example/Hyperskill/testeTernary.java
@@ -0,0 +1,58 @@
+package com.example.Hyperskill;
+
+public class testeTernary {
+    public static void main(String[] args){
+//        int a = 20;
+//        int b = 10;
+//        int c = 50;
+//        int d = 50;
+////        String result = a==b ?"equal" :
+////                a>b ? "More" : "Less";
+////
+////        System.out.println(result);
+////
+////        if (a + b == c) {
+////            d = c;
+////            System.out.println(d);
+////        } else {
+////            d = 10;
+////            System.out.println(d);
+////        }
+//
+//
+//        boolean b1 = true;
+//        boolean b2 = false;
+//        boolean b3 = false;
+//        boolean result = (b1 && b2) ? b1 : (b2 || b3) ? b2 : b3;
+//        System.out.println(result);
+
+
+//        int a = 4;
+//        int b = ++a;
+////
+////        System.out.println(a);
+////        System.out.println(b);
+//        int a = 4;
+//        System.out.println(a++ + a);
+//        System.out.println(a);
+//        int a = 4;
+//        int b = a++;
+//        int c = --a + b++;
+//        int result = ++a - (c++ - b) + c;
+//
+//        System.out.println(result);
+//        int n = 10;
+//        n--;
+//        System.out.println(n++);
+//        int result = 5;
+//        int a = 1;
+//        int b = --a;
+//        a++;
+//        result += a + (--a / ++b);
+//        System.out.println(result);
+        int a = -1;
+        System.out.println(1 - a++);
+
+
+    }
+}
